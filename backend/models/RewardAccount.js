33a3e3a3fc70d1ea4c1931835aa5938e2482fe63
jfjannat18-jpg <jf.jannat18@gmail.com>
2026-01-03 01:31:10 +0600

    Update RewardAccount.js

--- backend/models/RewardAccount.js
+++ backend/models/RewardAccount.js
@@ -13,11 +13,18 @@ const rewardAccountSchema = new mongoose.Schema({
   },
   level: {
     type: String,
+    enum: ['Bronze', 'Silver', 'Gold', 'Platinum'],  // Enum for predefined levels
     default: "Bronze"
   },
   referralCode: {
     type: String,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function(v) {
+        return /^[A-Za-z0-9]{6,10}$/.test(v);  // Regex for alphanumeric referral codes of 6-10 chars
+      },
+      message: props => `${props.value} is not a valid referral code!`
+    }
   },
   referredUsers: [{
     type: mongoose.Schema.Types.ObjectId,
@@ -30,4 +37,9 @@ const rewardAccountSchema = new mongoose.Schema({
   }]
 });
 
+// Add indexes for frequent querying
+rewardAccountSchema.index({ userId: 1 });
+rewardAccountSchema.index({ referralCode: 1 });
+
 module.exports = mongoose.model("RewardAccount", rewardAccountSchema);
+
